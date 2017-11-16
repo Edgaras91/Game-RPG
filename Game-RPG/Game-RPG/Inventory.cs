3ad7179d21c11a27ca1b26b49ceffd1aa55e8acb
Edgaras <32236075+Edgaras91@users.noreply.github.com>
2017-11-16 11:30:50 +0000

    Update Inventory.cs

--- Game-RPG/Game-RPG/Inventory.cs
+++ Game-RPG/Game-RPG/Inventory.cs
@@ -6,9 +6,24 @@ using System.Threading.Tasks;
 
 namespace Game_RPG
 {
-    class Inventory
+    public class Inventory
     {
         
+            
+        public static string[] inventory;
+        
+    }
+    public class list(){
+        
+        for (int i = 0; i < inventory.Length; i++) 
+        {
+            inventory[i] = i * i;
+        }
+        for (int i = 0; i < a.Length; i++) 
+        {
+            Console.WriteLine($"a[{i}] = {a[i]}");
+            
+        }
         public int smallPotions;
 
 
