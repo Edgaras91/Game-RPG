c23af5152d33de67e53cad896fd60dfedb67c1d6
Edgaras Valius <edgaras.valius@gmail.com>
2017-12-10 23:16:49 +0000

    HelloWorld page created, and edited.

--- Bradford_Index/Bradford_Index/App_Start/RouteConfig.cs
+++ Bradford_Index/Bradford_Index/App_Start/RouteConfig.cs
@@ -17,7 +17,13 @@ namespace Bradford_Index
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+              );
+
+
+            routes.MapRoute(
+            name: "Hello",
+            url: "{controller}/{action}/{name}/{id}"
+ );
         }
     }
 }
