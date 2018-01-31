dda80b2594425a1504c9770c1cd5c7eba9a5cbb9
Edgaras Valius <edgaras.valius@googlemail.com>
2018-01-31 15:06:55 +0000

    Porfolio fully deployed

---
+++ HideRugs/HideRugs/Content/Controllers/HideRugsController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HideRugs.Models;
+
+namespace HideRugs.Controllers
+{
+    public class HideRugsController : Controller
+    {
+        private HideRugsEntities db = new HideRugsEntities();
+
+        // GET: HideRugs
+        public ActionResult Index()
+        {
+            return View(db.Table_1.ToList());
+        }
+
+        // GET: HideRugs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table_1 table_1 = db.Table_1.Find(id);
+            if (table_1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table_1);
+        }
+
+        // GET: HideRugs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HideRugs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Image,Hide,Type,Size,Colour,Price,Stock,Description,ProductID,Additional")] Table_1 table_1)
+        { 
+            if (table_1.Image == null)
+            {
+                table_1.Image = "https://www.hiderugs.co.uk/wp-content/uploads/2017/02/logo.jpg";
+            }
+        
+        
+            if (ModelState.IsValid)
+            {
+                db.Table_1.Add(table_1);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(table_1);
+        }
+
+        // GET: HideRugs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table_1 table_1 = db.Table_1.Find(id);
+            if (table_1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table_1);
+
+        }
+
+        // POST: HideRugs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Image,Hide,Type,Size,Colour,Price,Stock,Description,ProductID,Additional")] Table_1 table_1)
+        {
+            if (table_1.Image == null)
+            {
+                table_1.Image = "https://www.hiderugs.co.uk/wp-content/uploads/2017/02/logo.jpg";
+            }
+            if (ModelState.IsValid)
+            {
+                db.Entry(table_1).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(table_1);
+        }
+
+        // GET: HideRugs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table_1 table_1 = db.Table_1.Find(id);
+            if (table_1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table_1);
+        }
+
+        // POST: HideRugs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Table_1 table_1 = db.Table_1.Find(id);
+            db.Table_1.Remove(table_1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
